fc0c5a010e5fa80b31de159bfe5b67d1455e9a34
Roey <Roey@LAPTOP-8EMDUF0H>
2019-02-25 07:26:27 +0200

    update

---
+++ BioType/src/main/java/keyboard_func/Start.java
@@ -0,0 +1,194 @@
+package keyboard_func;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+import java.util.Map.Entry;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import camera.Camera;
+import communication.AppAlert;
+import communication.Textnow;
+import functions.Functions;
+import gui.Gui;
+import lc.kra.system.keyboard.GlobalKeyboardHook;
+import lc.kra.system.keyboard.event.GlobalKeyAdapter;
+import lc.kra.system.keyboard.event.GlobalKeyEvent;
+import lock.Main;
+import objects.Node;
+
+public class Start implements Runnable{
+	//Functions fun=
+	public HashMap<String, Node> hmap = Main.hmap;
+    public String name ="";
+    public int type=1;
+	//----------------------------
+	public   Queue<String[]> fifo = Main.fifo;
+	public static boolean run = true;
+	public static String lastname="";
+	public static void main(String[] args) throws IOException {
+		// might throw a UnsatisfiedLinkError if the native library fails to load or a RuntimeException if hooking fails
+		//Runtime.getRuntime().exec("Rundll32.exe powrprof.dll,SetSuspendState Sleep");
+		Node n=new Node();
+		n.setName("Roey");
+		n.setTextuser("roey.sdomi");
+		Start s=new Start();
+		s.hmap.put("111111", n);
+		
+		AppAlert m1=new AppAlert();  
+		Thread t1 =new Thread(m1);  
+		t1.start(); 
+		/*
+		KeyboardFunc ro=new KeyboardFunc();
+	    ro.reckeyboard();
+	    
+	    ro.startkeyboard();
+		*/
+	}
+	public void run()
+	{
+		startkeyboard();
+	}
+    public void startkeyboard()
+	{
+		run=true;
+			fifo=new CircularFifoQueue<String[]>(8);
+			GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(true); // use false here to switch to hook instead of raw input
+
+			System.out.println("Global keyboard hook successfully started, press [escape] key to shutdown. Connected keyboards:");
+			for(Entry<Long,String> keyboard:GlobalKeyboardHook.listKeyboards().entrySet())
+				System.out.format("%d: %s\n", keyboard.getKey(), keyboard.getValue());
+			
+			keyboardHook.addKeyListener(new GlobalKeyAdapter() {
+				@Override public void keyPressed(GlobalKeyEvent event) {
+					
+					//System.out.println(event);
+					
+					
+				}
+				@Override public void keyReleased(GlobalKeyEvent event) 
+				{       
+					
+					fifo.add(keypress(event));
+					String mako=getstr(fifo);
+					if(mako.equals("qqqqqqqa")) {Functions.savecookies(lastname);}
+						if(mako.length()==8&hmap.containsKey(mako))
+						{     
+							
+							
+							if(hmap.get(mako).getTime()>timediff())
+							{
+								System.out.println("detect");
+								try {
+									 if(hmap.get(mako).getType()==1)
+									 {
+										 lastname=hmap.get(mako).getName();
+										 Functions.chromebyname(hmap.get(mako).getName());
+										 Gui.textArea.append("\n \n DETECTED !\n");
+									 }
+									
+								} catch (IOException |InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} 
+								
+							}
+							else
+							{
+								
+								try 
+								{
+									if(hmap.get(mako).getType()==2)
+									{
+										Functions.killchrome();
+										TimeUnit.SECONDS.sleep(2);
+										Functions.deletecookies();
+									}
+									Textnow r=new Textnow();
+									r.sendmess(hmap.get(mako).getTextuser(), Camera.takepic());
+									r.sendmess(hmap.get(mako).getTextuser(), hmap.get(mako).getName()+", SOMEONE TRYING TO HACK YOU");
+								} 
+								catch (IOException | InterruptedException e1)
+								{
+									// TODO Auto-generated catch block
+									
+								}
+								
+							}
+							
+						}
+						
+					
+					 
+				}
+			});
+		
+			try {
+				while(run) Thread.sleep(128);
+				if(!run) {keyboardHook.shutdownHook();}
+			} catch(InterruptedException e) { /* nothing to do here */ }
+			  finally { keyboardHook.shutdownHook(); }
+		
+	}
+	
+	public static String getstr(Queue<String[]> b)
+	{
+		String a="";
+		
+		for(String[] d:b)
+		{
+			a=a+d[1];
+		}
+		return a;
+	}
+	public static double gettime()
+	{
+		int a=Calendar.getInstance().get(Calendar.SECOND);
+		int b=Calendar.getInstance().get(Calendar.MILLISECOND);
+		double c=Double.valueOf(a+"."+b);
+		return c;
+		
+	}
+    public static String[] keypress(GlobalKeyEvent event)
+    {
+    	String a=String.valueOf(gettime());
+		String b=String.valueOf(event.getKeyChar());
+		String[]c= new String[2];
+		c[0]=a;
+		c[1]=b;
+		return c;
+    	
+    }
+    public  double timediff()
+    {
+    	int counter=1;
+    	double a=0;
+    	double b=0;
+    	for(String []g:fifo)
+    	{
+    		System.out.println(g[0]);
+    		if(counter==1) {a=Double.valueOf(g[0]);}
+    		if(counter==8) {b=Double.valueOf(g[0]);}
+    		counter++;
+    	}
+    	double c=0;
+    	System.out.println("this is a:"+a+"this is b: "+b);
+    	if(b>a)
+    	{c=a-b;}
+    	if(a>b)
+    	{
+    		c=a-b;
+    	    
+    	    c=60-c;
+    	}
+    	
+    	if(c<0) {c=c*-1;}
+    	System.out.println(c);
+    	
+    	return c;
+    }
+
+}
---
+++ BioType/src/main/java/lock/Main.java
@@ -0,0 +1,77 @@
+package lock;
+
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Queue;
+
+import org.apache.commons.collections4.queue.CircularFifoQueue;
+
+import communication.AppAlert;
+import functions.Save;
+import gui.Gui;
+import gui.Popup;
+import objects.Node;
+
+public class Main {
+     
+	public static File source = new File("C:\\Users\\Roey\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies");
+	public static HashMap<String, Node> hmap = new HashMap<String, Node>();
+	public static Queue<String[]> fifo = new CircularFifoQueue<String[]>(8); 
+	public static String manager="";
+	static boolean firsttime=false;
+	public static void main(String[] args) {
+		
+		Save s=new Save();
+		try {
+			if(new File("myObjects").exists())
+			Main.hmap=(HashMap<String, Node>) s.readobj_byname("myObjects");
+		
+		} catch (IOException | ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		File f1 = new File("source");
+		File f2 = new File("m");
+		Popup popup = new Popup();
+		if(!(f1.exists()&f2.exists()))
+		{
+		
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						firsttime=true;
+						
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+		
+		//-----------------
+		AppAlert m1=new AppAlert();  
+		Thread t1 =new Thread(m1);  
+		t1.start(); 
+		//-----------------
+		Gui window = new Gui();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					window.frame.setVisible(true);
+					if(firsttime) {
+					popup.frame.setVisible(true); firsttime=false;
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
