3988623ebd68b9d9fa3de77e92b1117dce75c77f
Roey <Roey@LAPTOP-8EMDUF0H>
2019-03-01 01:55:43 +0200

    doc

--- src/main/java/communication/Textnow.java
+++ src/main/java/communication/Textnow.java
@@ -11,23 +11,12 @@ import java.util.Scanner;
 import javax.net.ssl.HttpsURLConnection;
 
 public class Textnow {
+	/**
+	 * an Api class i created for app called "TEXTNOW"
+	 */
      public static String Cookie="unsupported_browsers_notif=true; _ga=GA1.2.1665469293.1550428349; _gid=GA1.2.4927981.1550428349; language=he; _fbp=fb.1.1550428351716.137431393; G_ENABLED_IDPS=google; fbm_134162068005=base_domain=.textnow.com; UserDidVisitApp=true; FirehoseSession-messaging=true; puntCookie=true; __rtgt_sid=js990pincs45xw; d7s_uid=js990pincs45xw; __gads=ID=0c819fee36be8d23:T=1550428376:S=ALNI_Maa_XbQe4jiiw58kmwzIXLrqXJk5A; PermissionPriming=1; FirehoseSession-portal=true; fbsr_134162068005=-xKrSrPZRdbkxdawrOPqK0HktOng00mEMcy8ZNPfHxA.eyJ1c2VyX2lkIjoiMTAwMDAwMTQwNzM1MDYwIiwiY29kZSI6IkFRQVhoSjRYZkpIc3BCUlREdGl0RVlRSzF3RkMtelBxYkp1QTNNLTVfT29JdlBtZ1pOODZTMTg5OGJ6SUwtZlpLWlU1UzhZNDdvU0dnOTRBYTQ3X0lGVjlqWkg0Q2ltQjdzZGR6RmlpVnJKeWVSMmJEdUpCbmRORmlSc050ekItSzYtcnFyaGtoazliODVRUDFTRjhob0NmZkJyZEVqcEtYWTJCMzQtVHFmNEh0RTZuMUwtbWdLVWVvbVdiV3J3VW5EWHlPclZILVc2ck9QakxuNTRyMVlMMThUYno4T19HR3V3VUE0Y3dzU08yTTQ5emY5aUtqODB4VzJhVk9LbWlSUG5KeUJoWUdHVmd4UTBhT0RzT24wMVhPY3dKWWlLTEdmcEVqU3BfT3BheGJmd1VRWVk3eGNnREpVNW5JVXJ2UDdqdDZBYTJBWUU1bXRKVnFKU2JHTzBWIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE1NTA0MjkyNDJ9; sm_dapi_session=1; connect.sid=s%3ATQiPYu1MHy7ACWL5l2WKocO0T7pGtwfK.O61Klu0hFB3amke0sJTUtK8Mss6m9%2FzduiVLhaT%2FCGI; stc117823=tsa:1550428357822.1955583937.3724961.1831161290579808.:20190217192531|env:1%7C20190320183237%7C20190217192531%7C17%7C1073241:20200217185531|uid:1550428357821.575358341.9570189.117823.55585102.82:20200217185531|srchist:1073241%3A1%3A20190320183237:20200217185531; d7s_spc=9; XSRF-TOKEN=cMNVVblc-blXaB7KQI41sTK-tqSVdZRX6C_Q" ;
      public static String res="";
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		
-		
-		Textnow t= new Textnow();
-	//	t.sendmess("roey.sdomi", "**Someone is trying to hack you**");
-        
 	
-		String []g=t.readlastmess();
-		System.out.println(g[0]);
-		System.out.println(g[1]);
-		
-		
-
-	}
 
 	
 	
@@ -46,7 +35,12 @@ public class Textnow {
 	}
 	
 	public String [] readlastmess() throws IOException
-	{
+	{    
+		/**
+		 * read last messege and user name
+		 */
+		
+		
 		String url="https://www.textnow.com/api/users/roeytest/messages?start_message_id=0&direction=future&page_size=0";
 		getreq(url);
 		int s=res.lastIndexOf("message\":\"");
@@ -60,12 +54,16 @@ public class Textnow {
 	}
 	public void sendmess(String to,String content) throws IOException
 	{
+		/**
+		 * send messges to user by name and content
+		 */
+		
 		String url="https://www.textnow.com/api/users/roeytest/messages";
 		String body ="json={\"contact_value\":\"roey.sdomi\",\"contact_type\":2,\"message\":\"efggflon\",\"read\":1,\"message_direction\":2,\"message_type\":1,\"from_name\":\"\",\"has_video\":false,\"new\":true,\"date\":\"2018-11-09T02:36:32.533Z\"}";
 		body=body.replace("roey.sdomi", to).replaceAll("efggflon", content);
 		postreq(url, body);
 	}
-	///-------------------------
+	///-----------http headers commands--------------
 	 public   void getreq(String enterurl) throws IOException 
 	  {
 		 
